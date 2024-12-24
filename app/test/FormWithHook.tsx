00582c82fd36a868dda38aa33af9eaf6ec49fcc1
Jonatan <jonatanjmissora@gmail.com>
2024-12-24 02:00:10 -0300

    arreglando tipado de prevState del FormWithHook y formState.prevState y de formState.errors

--- app/test/FormWithHook.tsx
+++ app/test/FormWithHook.tsx
@@ -1,50 +1,61 @@
 "use client"
 
-import { useActionState, useState } from "react"
+import { useActionState, useRef, useState } from "react"
 import { addTodo } from "./actions"
 import toast from "react-hot-toast"
-import { todoSchema } from "./todo.schema"
+import { todoSchema, TodoType } from "./todo.schema"
+
+type ResType = {
+  success: boolean,
+  prevState: {id: number, content: string},
+  errors: {id: string, content: string}
+} 
 
 export default function FormWithHook() {
 
-  const [errors, setErrors] = useState<{ [key: string]: string }>({ id: "", content: "" })
-  const [formState, formAction, isPending] = useActionState(async (prevState, formData: FormData) => {
-    setErrors({ id: "", content: "" })
+  const formRef = useRef<HTMLFormElement>(null)
+  const [formState, formAction, isPending] = useActionState(async (prevState: ResType, formData: FormData) => {
     const newTodo = Object.fromEntries(formData.entries())
+
+    const responseObj = {
+      success: true,
+      prevState: newTodo as TodoType,
+      errors: {id: "", content: ""}
+    }
 
     //client validation
     const { success, data, error } = todoSchema.safeParse({ ...newTodo, id: Number(newTodo.id) })
     if (!success) {
       const { id: idError, content: contentError } = error.flatten().fieldErrors
-      setErrors({
-        id: idError ? idError[0] : "",
-        content: contentError ? contentError[0] : ""
-      })
+      responseObj.errors = { id: idError ? idError[0] : "", content: contentError ? contentError[0] : "" }
       toast.error("Error Cliente")
-      return {
-        success: false,
-        prevState: newTodo,
-      }
+      return responseObj
     }
     toast.success("Success Ciente")
 
     const serverResult = await addTodo(data)
-    console.log({ serverResult })
+    //server validation
     if (!serverResult?.success && serverResult?.errors) {
       toast.error("Error Servidor")
+      responseObj.errors = serverResult.errors
+      return responseObj
     }
+    return {success: true,
+      prevState: {id: null, content: ""},
+      errors: {id: "", content: ""}}
 
   }, null)
 
   return (
-    <form action={formAction} className='flex gap-4 flex-col p-4 border m-4'>
+    <form ref={formRef} action={formAction} className='flex gap-4 flex-col p-4 border m-4'>
+      {JSON.stringify(formState)}
       <h2 className='text-2xl font-bold tracking-wide'>Formulario 2</h2>
 
       <input className="input input-primary" type="number" name="id" defaultValue={formState?.prevState?.id} />
-      <p>{errors?.id && errors.id}</p>
+      <p>{formState?.errors?.id && formState?.errors.id}</p>
 
       <input className="input input-primary" type="text" name="content" defaultValue={formState?.prevState?.content} />
-      <p>{errors?.content && errors.content}</p>
+      <p>{formState?.errors?.content && formState?.errors.content}</p>
 
       <button className='btn btn-primary' type="submit" disabled={isPending}>{isPending ? "..." : "Crear"}</button>
     </form>

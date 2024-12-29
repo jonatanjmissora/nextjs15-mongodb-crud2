7af01068cb29f2c72f2b8cec1e3c1ed4965f8e49
Jonatan <jonatanjmissora@gmail.com>
2024-12-28 22:04:02 -0300

    arreglando en la notebook

--- _components/Header.tsx
+++ _components/Header.tsx
@@ -8,14 +8,12 @@ export default async function Header() {
   const user = await getUserFromCookie()
 
   return (
-    <div className="navbar bg-slate-900 px-4">
-      <div className="flex-1">
-        <Link href={"/"} className="btn btn-ghost text-xl">K@to</Link>
-      </div>
+    <div className="navbar flex justify-between bg-slate-900 px-4">
+      <Link href={"/"} className="btn btn-ghost text-xl">K@to</Link>
       <nav>
-        <Link href={"/testSimpleClientAction"} className="btn btn-ghost">Simple Client Action</Link>
-        <Link href={"/testSimpleClientActionAndReactHookForm"} className="btn btn-ghost">Simple Client Action + React Hook Form</Link>
-        <Link href={"/testWithUseActionStateAndHook"} className="btn btn-ghost">Use Action State</Link>
+        <Link href={"/01testSimpleClientAction"} className="btn btn-ghost">Simple Client Action</Link>
+        <Link href={"/02testSimpleClientActionAndReactHookForm"} className="btn btn-ghost">Simple Client Action + React Hook Form</Link>
+        <Link href={"/04testWithUseActionStateAndHook"} className="btn btn-ghost">Use Action State</Link>
       </nav>
       <div className="flex-none">
         {
